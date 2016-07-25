97701f48d46777645f585a6dec8f5a41d309d9ee
johnmberger <johnmartinberger@gmail.com>
2016-07-25 10:08:36 -0600

    completed exercise

--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,24 @@ var seed = [
 // ** dom manipulation ** //
 
 $(document).on('ready', function() {
+  // Post seeded to-dos to the dom
+  for (var i = 0; i < seed.length; i++) {
+    appendToDom(seed[i]);
+  }
+  // Grab new to-dos from the form and post to the dom
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+    var $newListItem = $('#todoInput').val();
+    appendToDom($newListItem);
+    $('form').trigger('reset');
+  });
 
+  $('ol').on('click', 'span', function() {
+    $(this).parent().remove();
+  });
 });
 
 // ** helper functions ** //
+function appendToDom(seed) {
+  $('#all-todos').append('<li>' + seed + ' <span class="glyphicon glyphicon-remove-sign"><span></li>');
+}
